9bf2ca13249f4c4c48392816d1e28efe5e04fce9
Jerry Ai <aijl2008@qq.com>
2018-03-26 20:08:05 +0800

    Create pdo_persistent.php

---
+++ pdo_persistent.php
@@ -0,0 +1,24 @@
+<?php
+
+$host = isset($_GET["host"])?$_GET["host"]:’127.0.0.1’;
+$user = isset($_GET["user"])?$_GET["user"]:’root’;
+$pass = isset($_GET["password"])?$_GET["password"]:’123456’;
+$db = isset($_GET["db"])?$_GET["db"]:’test’;
+$table = isset($_GET["table"])?$_GET["table"]:’table’;
+
+$cursor = "cr_123456";
+
+try
+{
+  $dbh = new PDO("mysql:host=$host;port=5432;dbname=$db;user=$user;password=$pass", array(
+    PDO::ATTR_PERSISTENT => true
+));
+ foreach($dbh->query('SELECT * from '.$table .’ limit 100’) as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
