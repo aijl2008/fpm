bbf00a7d07d155e036e53b73922320176c30af5c
Jerry Ai <awz@awz.cn>
2018-03-26 22:02:55 +0800

    up

--- curl.php
+++ curl.php
@@ -1,13 +1,13 @@
 <?php
-$url = isset($_GET["url"])?$_GET["url"]:’http://127.0.0.1’;
-$params=['name'=>'John', 'surname'=>'Doe', 'age'=>36)
+$url = isset($_GET["url"]) ? $_GET["url"] : 'http://127.0.0.1';
+$params = ['name' => 'John', 'surname' => 'Doe', 'age' => 36];
 $defaults = array(
-CURLOPT_URL => $url , 
-CURLOPT_POST => true,
-CURLOPT_POSTFIELDS => $params,
-CURLOPT_HEADER => true,
-    CURLOPT_RETURNTRANSFER => false,
-    CURLOPT_TIMEOUT => 2
+    CURLOPT_URL => $url,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $params,
+    CURLOPT_HEADER => true,
+    CURLOPT_RETURNTRANSFER => false,
+    CURLOPT_TIMEOUT => 2
 );
 $ch = curl_init();
 curl_setopt_array($ch, ($options + $defaults));
--- pdo.php
+++ pdo.php
@@ -1,22 +1,16 @@
 <?php
-
-$host = isset($_GET["host"])?$_GET["host"]:’127.0.0.1’;
-$user = isset($_GET["user"])?$_GET["user"]:’root’;
-$pass = isset($_GET["password"])?$_GET["password"]:’123456’;
-$db = isset($_GET["db"])?$_GET["db"]:’test’;
-$table = isset($_GET["table"])?$_GET["table"]:’table’;
-
-$cursor = "cr_123456";
-
-try
-{
-  $dbh = new PDO("mysql:host=$host;port=5432;dbname=$db;user=$user;password=$pass");
- foreach($dbh->query('SELECT * from '.$table .’ limit 100’) as $row) {
-        print_r($row);
-    }
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
+$host = isset($_GET["host"]) ? $_GET["host"] : "127.0.0.1";
+$user = isset($_GET["user"]) ? $_GET["user"] : "root";
+$pass = isset($_GET["password"]) ? $_GET["password"] : "123456";
+$db = isset($_GET["db"]) ? $_GET["db"] : "test";
+$table = isset($_GET["table"]) ? $_GET["table"] : "table";
+try{
+    $dbh = new PDO("mysql:host=$host;port=3306;dbname=$db;user=$user;password=$pass");
+    foreach ($dbh->query("select * from $table limit 100") as $item){
+        print_r($item);
+    }
+    $dbh = null;
 }
-
+catch (PDOException $e){
+    echo $e->getMessage();
+}
