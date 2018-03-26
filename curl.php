d425b7618781feaab3ca3183f58725cd09b3fa67
Jerry Ai <aijl2008@qq.com>
2018-03-26 20:22:12 +0800

    Create curl.php

---
+++ curl.php
@@ -0,0 +1,14 @@
+<?php
+$url = isset($_GET["url"])?$_GET["url"]:’http://127.0.0.1’;
+$params=['name'=>'John', 'surname'=>'Doe', 'age'=>36)
+$defaults = array(
+CURLOPT_URL => $url , 
+CURLOPT_POST => true,
+CURLOPT_POSTFIELDS => $params,
+CURLOPT_HEADER => true,
+    CURLOPT_RETURNTRANSFER => false,
+    CURLOPT_TIMEOUT => 2
+);
+$ch = curl_init();
+curl_setopt_array($ch, ($options + $defaults));
+?>
